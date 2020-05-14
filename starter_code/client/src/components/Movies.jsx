c0d7517c878bb4a440c79b179745f181c1d976ce
Cule219 <culaficstefan@gmail.com>
2020-05-14 10:35:42 -0400

    iteration 4

--- starter_code/client/src/components/Movies.jsx
+++ starter_code/client/src/components/Movies.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import Axios from 'axios'
+import React, { Component } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
 class Movies extends Component {
   state = {
-    movies: []
-  }
+    movies: [],
+  };
 
-  async componentDidMount(){
-    let res = await Axios.get(`http://localhost:5000/movies`)
-    
+  async componentDidMount() {
+    let res = await Axios.get(`http://localhost:5000/movies`);
+
     this.setState({
-      movies:res.data.movies
-    })
+      movies: res.data.movies,
+    });
   }
 
   showMovies = () => {
-
-    return this.state.movies.map(eachMovie => {
+    return this.state.movies.map((eachMovie) => {
       return (
-      <li key={eachMovie._id}>
+        <li key={eachMovie._id}>
           <h1>{eachMovie.title}</h1>
           <h2>{eachMovie.director}</h2>
-          <img src={eachMovie.image} />
-          <p>{eachMovie.description}</p>
-      </li>
-      )
-    })
-
-  }
-
+          <Link to={`/movies/${eachMovie._id}`}>
+            <img src={eachMovie.image} />
+          </Link>
+          {/* <p>{eachMovie.description}</p> */}
+        </li>
+      );
+    });
+  };
 
   render() {
     return (
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Movie = require('../models/Movie')
+const Movie = require("../models/Movie");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -8,10 +8,16 @@ router.get("/", (req, res, next) => {
   res.json({ message: "index" });
 });
 
-router.get('/movies', (req, res) => {
-  Movie.find().then(movies => {
-    res.json({movies})
-  })
-})
+router.get("/movies", (req, res) => {
+  Movie.find().then((movies) => {
+    res.json({ movies });
+  });
+});
+
+router.get("/movies/:id", (req, res) => {
+  Movie.findById(req.params.id).then((movie) => {
+    res.json(movie);
+  });
+});
 
 module.exports = router;
